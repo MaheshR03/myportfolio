4ad8449932353f0d6854dc02e1f841dbeb43b976
Mahesh R <maheshrroy@gmail.com>
2024-11-13 01:48:13 +0530

    updated my recent projects

--- app/(home)/components/Projects.tsx
+++ app/(home)/components/Projects.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SiHtml5, SiCss3, SiJavascript, SiPhp } from "react-icons/si";
+import { SiHtml5, SiCss3, SiJavascript, SiPhp, SiReact, SiPython, SiMongodb, SiExpress, SiNodedotjs, SiTailwindcss } from "react-icons/si";
 import Title from "./Title";
 import Link from "next/link";
 import { DirectionAwareHover } from "../../../components/ui/direction-aware-hover";
@@ -11,14 +11,56 @@ export default function Projects(){
             title: "AI-driven bot for tailored customer service and effective sales engagement",
             tech: [SiHtml5, SiCss3, SiJavascript, SiPhp],
             link: "https://github.com/Ad1tyaNarayana/hacktathon-HAL04",
-            cover: "/project1.png",
+            cover: "/AI_chatbot.png",
+            background: "bg-indigo-500",
+        },
+        {
+            title: "Equihealth - Visualization of healthcare disparity across india using a prediction model",
+            tech: [SiReact, SiPython, SiHtml5, SiCss3],
+            link: "https://github.com/MaheshR03",
+            cover: "/Equihealth.png",
+            background: "bg-green-500",
+        },
+        {
+            title: "A MERN stack Netflix Clone styled with tailwindCSS",
+            tech: [SiReact, SiJavascript, SiMongodb, SiExpress, SiNodedotjs, SiTailwindcss],
+            link: "https://netflix-clone-lkqt.onrender.com",
+            cover: "/Netflix_clone.png",
+            background: "bg-indigo-500",
+        },
+        {
+            title: "Caffiend - A coffee tracking app with ReactJS, FantaCSS and Firebase",
+            tech: [SiReact, SiJavascript, SiHtml5, SiTailwindcss],
+            link: "https://caffiend-coffeetracker.vercel.app/",
+            cover: "/Caffiend.png",
+            background: "bg-green-500",
+        },
+        {
+            title: "Freescribe - React web based transcription & translation app that uses web workers to run ML models in the browser",
+            tech: [SiReact, SiJavascript, SiHtml5, SiTailwindcss],
+            link: "https://free-scribe-alpha.vercel.app/",
+            cover: "/Freescribe.png",
+            background: "bg-indigo-500",
+        },
+        {
+            title: "Pokédex - A ReactJS Pokédex styled with FantaCSS",
+            tech: [SiReact, SiJavascript, SiHtml5, SiTailwindcss],
+            link: "https://pokedex-pokemon-list.vercel.app/",
+            cover: "/Pokedex.png",
+            background: "bg-green-500",
+        },
+        {
+            title: "fitforge - The ultimate gym training app built with ReactJS and tailwindCSS",
+            tech: [SiReact, SiJavascript, SiHtml5, SiTailwindcss],
+            link: "https://fitforge-seven.vercel.app/",
+            cover: "/Fitfforge.png",
             background: "bg-indigo-500",
         },
         {
             title: "Lego Uni - Responsive University Website",
-            tech: [SiHtml5, SiCss3, SiPhp],
+            tech: [SiHtml5, SiCss3],
             link: "https://maheshr03.github.io/My-First-Website/",
-            cover: "/project2.png",
+            cover: "/Lego_uni.png",
             background: "bg-green-500",
         }
     ];
